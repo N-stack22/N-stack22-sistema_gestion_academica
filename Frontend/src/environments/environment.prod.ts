9c291cee199c7eba7118d4823941f3e34558728a
Nathalie-Rodriguez <72639576@continental.edu.pe>
2026-06-21 16:31:27 -0500

    Configura despliegue del frontend en GitHub Pages

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://colegio-horizonte-backend.up.railway.app',
+  apiUrl: 'https://n-stack22-sistemagestionacademica-production.up.railway.app',
 };
